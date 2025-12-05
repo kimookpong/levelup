8e16c1036cfcf4919131328e5ec206a444e09380
Hakim Mudor <78400565+kimookpong@users.noreply.github.com>
2025-12-05 20:26:12 +0700

    Update GameCard.tsx

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -17,7 +17,7 @@ export default function GameCard({ game }: { game: GameProps }) {
         <Link href={`/${game.slug}`} className="group relative block h-full">
             <div className="relative h-full w-full overflow-hidden rounded-xl border border-white/5 bg-[#1a1b26] transition-all duration-300 hover:border-primary/50 hover:shadow-[0_0_20px_rgba(255,0,85,0.2)] hover:-translate-y-1">
                 {/* Image Container */}
-                <div className="relative aspect-square w-full overflow-hidden">
+                <div className="relative aspect-[2/3] w-full overflow-hidden">
                     <Image
                         src={game.imageUrl}
                         alt={game.name}
